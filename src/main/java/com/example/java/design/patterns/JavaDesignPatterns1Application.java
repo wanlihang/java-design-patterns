c9aef866d32a041cc19cdab8099981b6eeaa2df5
mojue <wanlihang.wlh@alibaba-inc.com>
2024-12-16 14:53:22 +0800

    chore: 添加项目初始文件和配置文件

---
+++ src/main/java/com/example/java/design/patterns/JavaDesignPatterns1Application.java
@@ -0,0 +1,16 @@
+package com.example.java.design.patterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mojue
+ */
+@SpringBootApplication
+public class JavaDesignPatterns1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDesignPatterns1Application.class, args);
+    }
+
+}
